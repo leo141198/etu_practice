c09cf1f576cc2f8421f2354ddfa85b663e652018
Gerasimova Darina <gerasimova.darya96@gmail.com>
2018-06-29 21:23:57 +0300

    Добавлен класс Pair

---
+++ src/Pair.java
@@ -0,0 +1,21 @@
+public class Pair {
+    private Integer first;
+    private Integer second;
+
+    public Pair(Integer first, Integer second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Integer getFirst() {
+        return first;
+    }
+
+    public Integer getSecond() {
+        return second;
+    }
+
+    public static Pair of(Integer first, Integer second) {
+        return new Pair(first, second);
+    }
+}
