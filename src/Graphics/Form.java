dd6890125ebebf7098452586a0975ff1e8c3d65b
Leonid Kutsenok <leo@sochi.com>
2018-06-30 22:33:28 +0300

    Изменения главной формы

--- src/Graphics/Form.java
+++ src/Graphics/Form.java
@@ -2,21 +2,34 @@ package Graphics;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Form extends JFrame {
     public JPanel formView;
     private JButton button1;
     public JPanel map;
+    private JComboBox comboBox1;
+
+    private Map m;
 
     public Form() {
         setContentPane(formView);
         setVisible(true);
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new Dimension(300, 300));
+        setMinimumSize(new Dimension(600, 300));
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //m.timer.start();
+            }
+        });
     }
 
     private void createUIComponents() {
-        map = new Map();
+        m = new Map();
+        map = m;
     }
 }
