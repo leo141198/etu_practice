eecb268c204f4974d6dac7ff44cb9681d2dc239f
Leonid Kutsenok <leo@sochi.com>
2018-06-30 22:34:33 +0300

    Добавлен таймер для вызова следующей итерации сортировки

--- src/Graphics/Map.java
+++ src/Graphics/Map.java
@@ -16,55 +16,85 @@ public class Map extends JPanel {
     private ArrayList<SortElem> elems = new ArrayList<SortElem>();
     private Integer max_value;
     private Sort sort;
+    private Timer timer;
+    private Integer delay;
 
     public Map() {
         ArrayList<Integer> list = new ArrayList<Integer>();
         Random random = new Random();
-        for(int i = 0; i < 200; i++) {
+        for (int i = 0; i < 100; i++) {
             list.add(random.nextInt(1000));
         }
-        init(list);
-        Timer timer = new Timer(10, new ActionListener() {
+        init(list, true);
+        timer = new Timer(30, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 nextStep();
             }
         });
-        timer.start();
     }
 
     @Override
     public void paint(Graphics g) {
         super.paintComponent(g);
         setBackground(new Color(255, 255, 255));
-        float elem_width = (float)getWidth() / elems.size();
+        float elem_width = (float) getWidth() / elems.size();
         float elem_height;
         for (int i = 0; i < elems.size(); i++) {
-            elem_height = (elems.get(i).getValue() / (float)max_value) * getHeight();
-            elems.get(i).setRectangle(i * elem_width, (float)getHeight(), elem_width, elem_height);
+            elem_height = (elems.get(i).getValue() / (float) max_value) * getHeight();
+            elems.get(i).setRectangle(i * elem_width, (float) getHeight(), elem_width, elem_height);
             elems.get(i).paint(g);
         }
     }
 
-    public void init(ArrayList<Integer> array) {
+    public void init(ArrayList<Integer> array, boolean rgb) {
         this.max_value = Collections.max(array);
         for (Integer num : array) {
             this.elems.add(new SortElem(num, 0, 0, 0, 0));
         }
         this.sort = new QuickSort(array);
         sort.sort();
+        if (rgb) {
+            setRGBcolors();
+        }
         repaint();
     }
 
-    public void nextStep() {
+    private void nextStep() {
         Pair pair;
-        if((pair = sort.nextStep()) != null) {
+        if ((pair = sort.nextStep()) != null) {
             swapElems(pair.getFirst(), pair.getSecond());
+        } else {
+            timer.stop();
         }
     }
 
-    public void swapElems(int i, int j) {
-        Collections.swap(elems, i,j);
+    private void swapElems(int i, int j) {
+        Collections.swap(elems, i, j);
         repaint();
+    }
+
+    private void setRGBcolors() {
+        for (int i = 0; i < sort.array.size(); i++) {
+            for (SortElem elem : elems) {
+                if (sort.array.get(i).equals(elem.getValue())) {
+                    if (i < sort.array.size() / 6) {
+                        elem.setColor(new Color(255, (255 / (sort.array.size() / 6) * (i % (sort.array.size() / 6))), 0));
+                    } else if (i < sort.array.size() / 6 * 2) {
+                        elem.setColor(new Color(255 - (255 / (sort.array.size() / 6) * (i % (sort.array.size() / 6))), 255, 0));
+                    } else if (i < sort.array.size() / 6 * 3) {
+                        elem.setColor(new Color(0, 255, (255 / (sort.array.size() / 6) * (i % (sort.array.size() / 6)))));
+                    } else if (i < sort.array.size() / 6 * 4) {
+                        elem.setColor(new Color(0, 255 - (255 / (sort.array.size() / 6) * (i % (sort.array.size() / 6))), 255));
+                    } else if (i < sort.array.size() / 6 * 5) {
+                        elem.setColor(new Color((255 / (sort.array.size() / 6) * (i % (sort.array.size() / 6))), 0, 255));
+                    } else if(i < sort.array.size() - 2) {
+                        elem.setColor(new Color(255, 0, 255 - (255 / (sort.array.size() / 6) * (i % (sort.array.size() / 6)))));
+                    } else {
+                        elem.setColor(new Color(255,0,0));
+                    }
+                }
+            }
+        }
     }
 }
--- src/Graphics/SortElem.java
+++ src/Graphics/SortElem.java
@@ -1,7 +1,6 @@
 package Graphics;
 
 import java.awt.*;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 public class SortElem extends Component {
@@ -10,32 +9,26 @@ public class SortElem extends Component {
     private float width;
     private float height;
     private Integer value;
+    public Color color;
 
     public SortElem(Integer value, float x, float y, float width, float height) {
         this.value = value;
+        this.color = new Color(190, 190, 190);
         setRectangle(x, y, width, height);
     }
 
-    public SortElem(SortElem other) {
-        this.value = other.value;
-        setRectangle(other.x, other.y, other.width, other.height);
+    public void setColor(Color color) {
+        this.color = color;
     }
 
     public Integer getValue() {
         return value;
     }
 
-    public Point2D.Float getCoordinates() {
-        return new Point2D.Float(x, y);
-    }
-
-    public void setCoordinates(Point2D.Float coordinates) {
-        this.x = coordinates.x;
-        this.y = coordinates.y;
-    }
 
     public void setRectangle(float x, float y, float width, float height) {
-        setCoordinates(new Point2D.Float(x, y));
+        this.x = x;
+        this.y = y;
         this.width = width;
         this.height = height;
     }
@@ -44,7 +37,7 @@ public class SortElem extends Component {
     @Override
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(new Color(141, 151, 255));
+        g2d.setColor(color);
         Rectangle2D r2d = new Rectangle2D.Float(x, y - height, width, height);
         g2d.fill(r2d);
         g.setColor(Color.black);
