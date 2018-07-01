4c36f230a89a620ab28c5d7c45d93ff67542d355
Leonid Kutsenok <leo@sochi.com>
2018-07-01 23:00:15 +0300

    Добавлено меню и изменена форма

--- src/Graphics/Form.java
+++ src/Graphics/Form.java
@@ -5,13 +5,18 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Form extends JFrame {
-    public JPanel formView;
-    private JButton button1;
-    public JPanel map;
-    private JComboBox comboBox1;
-
-    private Map m;
+public class Form extends JFrame implements ActionListener {
+    protected JPanel formView;
+    protected JButton button_pause;
+    protected JPanel map;
+    protected JSlider slider_delay;
+    private JButton button_start;
+    private JButton button_prev_step;
+    private JButton button_next_step;
+    private JButton button_next_auto;
+    private JButton button_prev_auto;
+    protected Menu menuBar;
+    protected Map m;
 
     public Form() {
         setContentPane(formView);
@@ -20,16 +25,37 @@ public class Form extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(600, 300));
 
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //m.timer.start();
-            }
-        });
+        button_start.addActionListener(this);
+        button_pause.addActionListener(this);
+
+        button_next_step.addActionListener(this);
+        button_prev_step.addActionListener(this);
+
+        button_next_auto.addActionListener(this);
+        button_prev_auto.addActionListener(this);
     }
 
     private void createUIComponents() {
         m = new Map();
         map = m;
+        menuBar = new Menu();
+        this.setJMenuBar(menuBar);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button_start || e.getSource() == button_next_auto) {
+            m.auto_direction = true;
+            m.timer.start();
+        } else if(e.getSource() == button_pause) {
+            m.timer.stop();
+        } else if(e.getSource() == button_next_step) {
+            m.nextStep();
+        } else if(e.getSource() == button_prev_step) {
+            m.prevStep();
+        } else if(e.getSource() == button_prev_auto) {
+            m.auto_direction = false;
+            m.timer.start();
+        }
     }
 }
---
+++ src/Graphics/Menu.java
@@ -0,0 +1,87 @@
+package Graphics;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu extends JMenuBar implements ActionListener {
+    private JMenu dataMenu;
+    private JMenu algorithmMenu;
+    private JMenu displayMenu;
+
+    private JMenuItem openFile;
+    private JMenuItem generate;
+
+    private JMenu selectColor;
+    private JRadioButtonMenuItem colorRGB;
+    private JRadioButtonMenuItem colorDemo;
+
+    public Menu() {
+        initDataMenu();
+        algorithmMenu = new JMenu("Алгоритм");
+        initDisplayMenu();
+        this.add(algorithmMenu);
+
+
+    }
+
+    private void initDataMenu() {
+        dataMenu = new JMenu("Данные");
+
+        openFile = new JMenuItem("Прочитать из файла");
+        dataMenu.add(openFile);
+
+        generate = new JMenuItem("Сгенерировать");
+        dataMenu.add(generate);
+
+        this.add(dataMenu);
+    }
+
+    private void initDisplayMenu() {
+        displayMenu = new JMenu("Отображение");
+        selectColor = new JMenu("Выбор цветов");
+
+        colorRGB = new JRadioButtonMenuItem("RGB");
+        colorRGB.setSelected(false);
+        colorRGB.addActionListener(this);
+        selectColor.add(colorRGB);
+
+        colorDemo = new JRadioButtonMenuItem("Учебные");
+        colorDemo.setSelected(true);
+        colorDemo.addActionListener(this);
+        selectColor.add(colorDemo);
+
+        displayMenu.add(selectColor);
+        this.add(displayMenu);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == colorRGB) {
+            if(colorRGB.isSelected()) {
+                setRGB(true);
+            } else {
+                setRGB(false);
+            }
+        } else if(e.getSource() == colorDemo) {
+            if(colorDemo.isSelected()) {
+                setRGB(false);
+            } else {
+                setRGB(true);
+            }
+        }
+    }
+
+    private void setRGB(boolean rgb) {
+        Form parent = (Form)SwingUtilities.getWindowAncestor(this);
+        if(rgb) {
+            colorDemo.setSelected(false);
+            colorRGB.setSelected(true);
+            parent.m.setRGB(true);
+        } else {
+                colorDemo.setSelected(true);
+                colorRGB.setSelected(false);
+                parent.m.setRGB(false);
+        }
+    }
+}
