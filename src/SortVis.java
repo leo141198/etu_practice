f0f2c900c39866be07c76014be7e4e4b4cec8345
Gerasimova Darina <gerasimova.darya96@gmail.com>
2018-07-02 17:53:53 +0300

    Изменен основной класс программы

--- src/SortVis.java
+++ src/SortVis.java
@@ -1,23 +1,9 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import Graphics.Form;
 
 public class SortVis {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        Scanner keyboard = new Scanner(System.in);
-        while (keyboard.hasNextInt()) {
-            list.add(keyboard.nextInt());
-        }
-        Sort sort = new QuickSort(list);
-        Pair step;
-        sort.sort();
-        while ((step = sort.nextStep()) != null) {
-            System.out.println(step.getFirst() + " " + step.getSecond());
-        }
-        for(Integer i : sort.array) {
-            System.out.print(i + " ");
-        }
+        Form form = new Form();
     }
 
 }
